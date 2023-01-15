d5c0cee3c340a80d8776a6cf202ac11da33e3160
unknown <jake.h.marquez@gmail.com>
2023-01-14 23:17:24 -0800

    list all sheets

--- readme.txt
+++ readme.txt
@@ -1,2 +1,2 @@
-Begin by creating a node project with typescript, then implement express to run a server
-https://www.youtube.com/watch?v=Fqv2-f1bIls
+-install node
+-setup a project like https://www.youtube.com/watch?v=Fqv2-f1bIls
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express';
+const smartsheet = require('smartsheet');
 
 const app: Express = express();
 
@@ -6,6 +7,13 @@ app.listen(4443, () => {
     console.log('hello');
 });
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('hello')
+app.get('/', async (req: Request, res: Response) => {
+    const client = smartsheet.createClient({
+      accessToken: 'Wr4JhaEreWRNlClb7oeI0yqRDx2NuNMAEf191',
+      logLevel: 'info'
+    });
+    
+    const sheets = await client.sheets.listSheets();
+    console.log(sheets);
 });
+
